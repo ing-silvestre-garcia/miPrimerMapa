a6938ab1b0f27a2efa6643ade82196d9f28232ec
Silvestre Garcia <silver@Silvestres-iMac.local>
2017-09-27 19:41:49 -0500

    Se agrega funcionalidad de mapa

--- miPrimerMapa/Home.m
+++ miPrimerMapa/Home.m
@@ -23,18 +23,18 @@
 - (void)loadView {
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:20.6817814
+                                                            longitude:-103.4648342
+                                                                 zoom:16];
     GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView.myLocationEnabled = YES;
     self.view = mapView;
     
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
+    marker.position = CLLocationCoordinate2DMake(20.6817814, -103.4648342);
+    marker.title = @"Estadio Chivas";
+    marker.snippet = @"Zapopan";
     marker.map = mapView;
 }
 
